e3fedfe7a4ff9569e9d7724602d8a2175c9dc008
sampasha007 <sampasha007@gmail.com>
2017-12-10 17:15:04 +0530

    New Code
    ls
    
    exit()

--- icecream.go
+++ icecream.go
@@ -1,10 +1,48 @@
 package main
+import("fmt"
+	"time"
+)
 
-import "fmt"
+c1:= make(chan int)
+func clerk(cust_id int){
+	
+	fmt.Println("cust %d is being served" )
+	c1<- cust_id
+
+}
+
+func manager(){
+	seed(time.Now().seconds)
+	decision:= rand.Intn(2)
+	if decision==1{
+	id:<-c1
+	fmt.Println("Customer %d is selected", id)
+	c1<-id
+	}
+	else{
+	go clerk(id)
+	}
+}
+
+func cashier(){
+	cust<-c1
+	fmt.Println("Cash collected for cost %d" , cust)
+}
 
 func main(){
-
-
-	fmt.Println("ICECREAM SIMULATOR")
-	fmt.Println("===================\n\n")
+	var(
+	cust_id int=1
+	n int
+	)
+for{
+	fmt.Scanf(&n)
+	if n==1{
+	go clerk(cust_id)
+	go manager()
+	go wait()
+	go cashier()
+	cust_id+=1
+	}
+}
+else break
 }
